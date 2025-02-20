b399333eb120b6554657bb4495c2d47ee918f97e
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-20 15:17:25 +0900

    [fix]:"점수현황판 버그 고침"
    기록이 없을때 "저장된 기록이 없다"라고 떠야되는데 안떠서 고침
    저장값이 없어도 기본값이 0이라 발생한
    상황 베스트스코어의 존재여부부터 해쉬키로 확인후 메세지 출력하도록 수정함
    
    시작지점에 탭키 애니메이션 추가

--- Assets/Scripts/Manger/DisplayScore.cs
+++ Assets/Scripts/Manger/DisplayScore.cs
@@ -21,17 +21,24 @@ public class DisplayScore : MonoBehaviour
 
     void ShowPanel()
     {
-        int savedScore = PlayerPrefs.GetInt("BestScore", 0); // PlayerPrefs에서 점수 가져오기
-        if (savedScore == -1)
+        if (PlayerPrefs.HasKey("BestScore")) // "BestScore" 키가 존재하는지 확인
         {
-            scoreText.text = "저장된 기록이 없습니다.";
-            ScorePanel.SetActive(true);
+            int savedScore = PlayerPrefs.GetInt("BestScore", 0); // PlayerPrefs에서 점수 가져오기
+            if (savedScore == 0)
+            {
+                scoreText.text = "저장된 기록이 없습니다.";
+            }
+            else
+            {
+                scoreText.text = "점수: " + savedScore.ToString(); // UI 업데이트
+            }
         }
         else
         {
-            scoreText.text = "점수: " + savedScore.ToString(); // UI 업데이트
-            ScorePanel.SetActive(true); 
+            scoreText.text = "저장된 기록이 없습니다."; // PlayerPrefs에 값이 없을 때
         }
+
+        ScorePanel.SetActive(true); // 패널 활성화
     }
 
     void HidePanel()
